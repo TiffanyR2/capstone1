c276a19d96ed736c8fcb4cc1dd74e3ca55ac2f3f
Tiffanyr1 <166432598+Tiffanyr1@users.noreply.github.com>
2024-04-17 15:06:38 -0400

    update

---
+++ app.js
@@ -0,0 +1,15 @@
+let openShopping = document.querySelector('.shopping');
+let closeShopping =document.querySelector('.closeshopping');
+let list = document.querySelector('.list');
+let listCard = document.querySelector('.listCard');
+let body = document.querySelector('body');
+let total = document.querySelector('.total');
+let quantity = document.querySelector('.quantity');
+
+openShopping.addEventListener('click' ,()=>{
+   body.classList.add('active');
+})
+closeShopping.addEventListener('click' , ()=>{
+    body.classList.remove('active');
+})
+   
